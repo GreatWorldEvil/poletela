74d2ded29f99d4ed1d2159ad1f0361763838efe1
GreatWorldEvil <polamakarona999@gmai.com>
2024-05-06 14:11:01 +0300

    makarova polina

--- WpfApplication1/WpfApplication1/Program.cs
+++ WpfApplication1/WpfApplication1/Program.cs
@@ -21,69 +21,53 @@ class letychaya_fignya {
 
     public event MyEventHandler Earth;
 
-    public void poletela() {
+    public void poletela(double start_v, double alfa) {
         
-        Console.WriteLine("Введите ускорение, угол фигни: ");
+        /*Console.WriteLine("Введите ускорение, угол фигни: ");
         start_v = Convert.ToDouble(Console.ReadLine());
-        alfa  = Convert.ToDouble(Console.ReadLine());
+        alfa  = Convert.ToDouble(Console.ReadLine());*/
         
         double stena_x, h, w;
-        Console.WriteLine("Введите расстояние до стенки, высоту и ширину стенки: "); //создаём препятствие
 
-        stena_x = Convert.ToDouble(Console.ReadLine());
-        h = Convert.ToDouble(Console.ReadLine());
-        w = Convert.ToDouble(Console.ReadLine());
+        Random rnd = new Random();
+        stena_x = rnd.Next(0,100);
+        h = rnd.Next(0, 70);
+        w = rnd.Next(0, 20);
         
 
         for (double t = 0; t < 20  ; t += 0.1){
 
             double a = alfa* (Math.PI /180); // теперь в радианах 
             
-            x = 5 + start_v * Math.Cos(a) * t + 9.8 * t * t / 2;
+            x = 5 + start_v * Math.Cos(a) * t + 9.8 * t * t / 2;//вычисляем координаты х, у
             y = 5 + start_v * Math.Sin(a) * t;
 
-            Console.WriteLine("X : {0}  Y : {1} ", x, y);
+            //Console.WriteLine("X : {0}  Y : {1} ", x, y); кусок кода для проверки, не убирать
             alfa -= t;
             
             if (y <= 0)
             {
-                Earth(this, new EventArgs() );
+                Earth(this, new EventArgs() ); //вызываем событие, передаём ему аргументы
                 return;
             }
             if (y <= h && x > stena_x && x < (stena_x + w))
             {
-                Collision(this, new EventArgs());
+                Collision(this, new EventArgs()); //вызываем событие, передаём ему аргументы
                 return;
             }
         }
    }
-   
-   
     
 }
 
-/*class Stenka{ //класс, в котором будет обработчик события
-    
-public
 
-    double mainX, heightS, widthS;//расстояние до стенки, высота, ширина стенки соотв. стенка по умолчанию стоит на земле
-
-     //конструктор
-    public Stenka(double x, double h, double s){
-        mainX = x;
-        heightS = h;
-        widthS = s;
-    }
-
-    void Collision(){//обработчик события
-        Console.WriteLine("Ooooooooops! It was collision!");
-    }
-
-}*/
 
 public class Program : Window
 {
+    Grid grid;
+
     [STAThread]
+
     public static void Main()
     {
         Application app = new Application();
@@ -91,30 +75,92 @@ public class Program : Window
     }
     public Program()
     {
-        Title = "Нажатие кнопки"; //название окна
-        Button qestion = new Button(); //этим классом представлена кнопка со свойством Content и событием Click
+        Title = "Игрушка"; //название окна
+        
+        //Content = qestion; //сам объект Button задаётся свойству Content объекта Window
+
+        grid = new Grid(); //создаём грид
+        //grid.HorizontalAlignment = HorizontalAlignment.Left;
+        //grid.VerticalAlignment = VerticalAlignment.Top;
+        grid.Width = 400;
+        grid.Height = 250;
+        grid.ShowGridLines = true;
+
+        //создаём столбцы
+        ColumnDefinition colDef1 = new ColumnDefinition();
+        ColumnDefinition colDef2 = new ColumnDefinition();
+
+        //добавляем столбцы
+        grid.ColumnDefinitions.Add(colDef1);
+        grid.ColumnDefinitions.Add(colDef2);
+
+        //создаём строки
+        RowDefinition rowDef1 = new RowDefinition();
+        RowDefinition rowDef2 = new RowDefinition();
+        RowDefinition rowDef3 = new RowDefinition();
+
+        //добавляем строки
+        grid.RowDefinitions.Add(rowDef1);
+        grid.RowDefinitions.Add(rowDef2);
+        grid.RowDefinitions.Add(rowDef3);
+
+        //создаём, добавляем канвас с анимацией
+        Canvas picture = new Canvas();
+        Grid.SetRow(picture, 0);
+        Grid.SetColumn(picture, 1);
+
+        //создаём кнопку, добавляем в грид
+        Button qestion = new Button();
         qestion.Content = "Запуск?";  //свойству Content объекта Button задается текстовая строка
-        qestion.Click += ButtonOnClick;
-        Content = qestion; //сам объект Button задаётся свойству Content объекта Window
+        qestion.Click += ButtonOnClick; //задаём обработчик события
+        Grid.SetRow(qestion, 2);
+        Grid.SetColumn(qestion, 1);
+
+        grid.Children.Add(picture);
+        grid.Children.Add(qestion);
+
+        Content = grid;// делаем грид содержимым окна
     }
 
-    public void Ops_Collision(object sender, EventArgs e)
+    public void Ops_Collision(object sender, EventArgs e)//обработчик события "столкновение с препятствием"
     {
         Console.WriteLine("Ooooooops! Collision!");
         MessageBox.Show("Ooooooops! Collision!");
     }
 
-    public void Ops_Earth(object sender, EventArgs e)
+    public void Ops_Earth(object sender, EventArgs e)//обработчик события "падение на землю"
     {
         Console.WriteLine("There in no collision!");
         MessageBox.Show("There in no collision!");
     }
 
-    void ButtonOnClick(object sender, RoutedEventArgs args)
+    void ButtonOnClick(object sender, RoutedEventArgs args) //обработчик события нажатие на кнопку, создаёт объект класса "фигня"
     {
+        //создаём текстовое окно для ввода данных
+        TextBlock message = new TextBlock();
+        message.Text = "Введите начальную скорость и угол: ";
+        Grid.SetRow(message, 0);
+        Grid.SetColumn(message, 0);
+
+        TextBox velocity = new TextBox();
+        velocity.Text = "0";
+        Grid.SetRow(velocity, 1);
+        Grid.SetColumn(velocity, 0);
+
+        TextBox corner = new TextBox();
+        corner.Text = "0";
+        Grid.SetRow(corner, 2);
+        Grid.SetColumn(corner, 0);
+
+        grid.Children.Add(velocity);
+        grid.Children.Add(corner);
+        grid.Children.Add(message);
+
+        double start_v = Convert.ToDouble(velocity.Text);
+        double alfa = Convert.ToDouble(corner.Text);
         letychaya_fignya fruit = new letychaya_fignya();
-        fruit.Earth += Ops_Earth;
+        fruit.Earth += Ops_Earth;//назначаем обработчики события
         fruit.Collision += Ops_Collision;
-        fruit.poletela();
+        fruit.poletela(start_v,alfa);//запуск!
     }
 }
